bf77dccb06bb231f938fb1731a423457d643f077
Santosh Kumar <sntshkmr60@gmail.com>
2015-07-18 18:30:08 +0530

    Experimenting

--- placecamera.php
+++ placecamera.php
@@ -1,15 +1,18 @@
 <?php
-$my_img = imagecreate( 200, 80 );
-$background = imagecolorallocate( $my_img, 0, 0, 255 );
-$text_colour = imagecolorallocate( $my_img, 255, 255, 0 );
-$line_colour = imagecolorallocate( $my_img, 128, 255, 0 );
-imagestring( $my_img, 4, 30, 25, "PlaceCamera", $text_colour );
-imagesetthickness ( $my_img, 5 );
-imageline( $my_img, 30, 45, 165, 45, $line_colour );
+$extra_path = str_replace($_SERVER['SCRIPT_NAME'],'',$_SERVER['REQUEST_URI']);
+$parts = explode('/',$extra_path);
+
+$my_img = imagecreate( $parts[1], $parts[2] );
+$background = imagecolorallocate( $my_img, 128, 128, 128 );
+$text_colour = imagecolorallocate( $my_img, 55, 55, 55 );
+$text = 'Testing..';
+
+$font = 'Monaco_Linux.ttf';
+
+imagettftext($my_img, 20, 0, 100, 200, $text_colour, $font, $text);
 
 header( "Content-type: image/png" );
 imagepng( $my_img );
-imagecolordeallocate( $line_color );
 imagecolordeallocate( $text_color );
 imagecolordeallocate( $background );
 imagedestroy( $my_img );
