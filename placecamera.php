555cfda54ab3024547cd72d359f1b29161588e11
Santosh Kumar <sntshkmr60@gmail.com>
2015-07-20 23:22:22 +0530

    shows dimension of image
    Project now in a shape where it can show
    dimension of image. Camera yet to be
    implemented.

--- placecamera.php
+++ placecamera.php
@@ -2,14 +2,17 @@
 $extra_path = str_replace($_SERVER['SCRIPT_NAME'],'',$_SERVER['REQUEST_URI']);
 $parts = explode('/',$extra_path);
 
-$my_img = imagecreate( $parts[1], $parts[2] );
+$width = $parts[1];
+$height = $parts[2];
+
+$my_img = imagecreate( $width, $height );
 $background = imagecolorallocate( $my_img, 128, 128, 128 );
 $text_colour = imagecolorallocate( $my_img, 55, 55, 55 );
-$text = 'Testing..';
+$text = $height . ' x ' . $width;
 
 $font = 'Monaco_Linux.ttf';
 
-imagettftext($my_img, 20, 0, 100, 200, $text_colour, $font, $text);
+imagettftext($my_img, 20, 0, $width/2-70, $height/2, $text_colour, $font, $text);
 
 header( "Content-type: image/png" );
 imagepng( $my_img );
